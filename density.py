Introduce a small offset during linear rescaling and shifting to fix overflow issue

density.py
@@ -208,6 +208,7 @@ class DensityEstimate():
     def score_samples(self, X):
         assert len(X.shape) == 2, "X must be of shape (n_samples, n_features)"
         logpdf = np.zeros(X.shape[0])
+        X = X.astype(np.float32)
 
         if self.bounded:
             # First compute the log jacobian from logit transformation
@@ -215,7 +216,7 @@ class DensityEstimate():
             # Then perform the transformation
             X = self.transformation.logit_transform(X)
         
-        X_torch = torch.from_numpy(X.astype(np.float32)).to(self.device)
+        X_torch = torch.from_numpy(X).to(self.device)
 
         logpdf += self.model.log_probs(X_torch).detach().cpu().numpy()
         return logpdf
@@ -228,10 +229,14 @@ class DensityEstimate():
 
         # NOTE: always return samples to CPU
         with torch.no_grad():
-            generated_samples = self.model.sample(n_samples).detach().cpu().numpy().astype(np.float64)
+            generated_samples = self.model.sample(n_samples).detach().cpu().numpy().astype(np.float32)
 
         if self.bounded:
             # Perform inverse transform
             generated_samples = self.transformation.inverse_logit_transform(generated_samples)
+
+            # Replace inf and -inf with proper values
+            generated_samples = np.where(generated_samples == np.inf, self.transformation.upper_bounds, generated_samples)
+            generated_samples = np.where(generated_samples == -np.inf, self.transformation.lower_bounds, generated_samples)
 
         return generated_samples

transform.py
@@ -1,18 +1,24 @@
 import numpy as np
+from scipy.special import logit, expit
 
 class LogitTransform():
-    def __init__(self, lower_bounds=None, upper_bounds=None):
+    def __init__(self, lower_bounds=None, upper_bounds=None, _OFFSET=1e-4):
         if lower_bounds is not None and upper_bounds is not None:
             self.rescaled = True
-            _OFFSET = 0
-            self.lower_bounds = lower_bounds - _OFFSET
-            self.upper_bounds = upper_bounds + _OFFSET
+            self.lower_bounds = lower_bounds
+            self.upper_bounds = upper_bounds
+            self._OFFSET = _OFFSET
         else:
             self.rescaled = False
 
     @staticmethod
-    def rescale(x, lower_bounds, upper_bounds):
+    def rescale(x, lower_bounds, upper_bounds, _OFFSET=1e-4):
         x = np.asarray(x)
+
+        # Introduce a small offset to avoid overflow
+        x = np.where(x <= lower_bounds, lower_bounds+_OFFSET, x)
+        x = np.where(x >= upper_bounds, upper_bounds-_OFFSET, x)
+
         return (x - lower_bounds)/(upper_bounds-lower_bounds)
 
     @staticmethod
@@ -21,25 +27,25 @@ class LogitTransform():
         return (upper_bounds-lower_bounds)*x + lower_bounds
 
     def logit_transform(self, x):
-        x = np.asarray(x)
+        x = np.asarray(x, dtype=np.float128)
         if self.rescaled:
             # Rescale to (0, 1)
-            x = self.rescale(x, self.lower_bounds, self.upper_bounds)
-        logit_x = np.log(x) - np.log1p(-x)
-        return logit_x
+            x = self.rescale(x, self.lower_bounds, self.upper_bounds, self._OFFSET)
+        logit_x = logit(x)
+        return logit_x.astype(np.float32)
 
     def inverse_logit_transform(self, logit_x):
-        logit_x = np.asarray(logit_x)
-        x = 1./(1.+np.exp(-logit_x))
+        logit_x = np.asarray(logit_x, dtype=np.float64)
+        x = expit(logit_x)
         if self.rescaled:
             x = self.inverse_rescale(x, self.lower_bounds, self.upper_bounds)
-        return x
+        return x.astype(np.float32)
 
     def log_jacobian(self, x):
         lj = np.zeros(x.shape[0])
 
         if self.rescaled:
-            p = self.rescale(x, self.lower_bounds, self.upper_bounds)
+            p = self.rescale(x, self.lower_bounds, self.upper_bounds, self._OFFSET)
             lj -= np.ones(x.shape[0])*np.sum(np.log(self.upper_bounds - self.lower_bounds))
         
         lj -= np.sum(np.log(p) + np.log1p(-p), axis=1)