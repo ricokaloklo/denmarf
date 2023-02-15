251ed6fc1f7aa137f321bcd96265a590a681039f
Rico Lo <ka-lok.lo@ligo.org>
2023-02-15 15:21:53 -0800

    Add a routine for making corner plots

--- denmarf/utils.py
+++ denmarf/utils.py
@@ -1,4 +1,5 @@
 import torch
+from getdist import MCSamples, plots
 
 def determine_device(device, use_cuda):
     if use_cuda:
@@ -11,3 +12,11 @@ def determine_device(device, use_cuda):
         pytorch_device = torch.device(device)
 
     return pytorch_device
+
+def plot_corner(original_samples, density_estimate, parameter_labels=None, filename="corner_plot.png"):
+    samples_orig = MCSamples(samples=original_samples, label="original samples", names=parameter_labels)
+    samples_synth = MCSamples(samples=density_estimate.sample(n_samples=original_samples.shape[0]), label="generated samples", names=parameter_labels)
+
+    g = plots.get_subplot_plotter()
+    g.triangle_plot([samples_orig, samples_synth], filled=False)
+    g.export(filename)
