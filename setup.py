198617031054991edef789743827304456620661
Rico Lo <ka-lok.lo@ligo.org>
2023-05-18 23:34:38 -0700

    Bump to version 0.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='denmarf',
-      version='0.3',
+      version='0.3.1',
       description='Density EstimatioN using Masked AutoRegressive Flow',
       author='Rico Ka Lok Lo',
       author_email='rico.kaloklo@gmail.com',
