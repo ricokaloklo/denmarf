b5fc2d36615895ad965a611b9636f822894d1a0f
Rico Lo <ka-lok.lo@ligo.org>
2023-06-28 18:27:55 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from pathlib import Path
 
 setup(name='denmarf',
       version='0.3.2',
       description='Density EstimatioN using Masked AutoRegressive Flow',
+      long_description=Path("README.md").read_text(encoding="utf-8"),
+      long_description_content_type="text/markdown",
       author='Rico Ka Lok Lo',
       author_email='rico.kaloklo@gmail.com',
       packages=['denmarf'],
